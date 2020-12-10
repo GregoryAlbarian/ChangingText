c614dca58ecfbd903cb6950f5e3e9508d39a376c
GregoryAlbarian <greg1999@sbcglobal.net>
2020-12-10 15:50:54 -0800

    now working

--- part2.sh
+++ part2.sh
@@ -1,30 +1,31 @@
 #!/bin/bash
 
-count=0
+count=1
 
-while read directory; do
-  cd $directory
-  count=$[ $count + 1 ]
+cd ./project
+while [ $count -le 10 ]; do
+  cd ./$count
   last_digit=$[ $count % 10 ]
 
-  if [ last_digit == 1 ]
+  if [ $last_digit == 1 ]
   then
     sed -i '4s/.*/"eat beets"/g' stuff.txt
-  elif [ last_digit == 4 ]
+  elif [ $last_digit == 4 ]
   then
     sed -i '4s/.*/"squash are great"/g' stuff.txt
-  elif [ last_digit == 5 ]
+  elif [ $last_digit == 5 ]
   then
     sed -i '4s/.*/"dogs are better than cats"/g' stuff.txt
-  elif [ last_digit == 7 ]
+  elif [ $last_digit == 7 ]
   then
     sed -i '4s/.*/"hello world"/g' stuff.txt
-  elif [ last_digit == 0 ]
+  elif [ $last_digit == 0 ]
   then
     sed -i '4s/.*//g' stuff.txt
   else
     sed -i '4s/.*/"welcome to file stuff.txt"/g' stuff.txt
   fi
-
+  
+  count=$[ $count + 1 ]
   cd ..
-done <contents.txt
+done
